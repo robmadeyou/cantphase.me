91052582dc3cb7b0c30165abce247c5402767df6
robmadeyou <robmadeyou@gmail.com>
2015-04-08 23:43:34 +0100

    Starting work on a new filter for the all songs list, also trying to style it properly, so far no luck

---
+++ src/Presenters/Music/Browse/BrowseSearchView.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * How is it going yo
+ */
+
+namespace Cant\Phase\Me\Presenters\Music\Browse;
+
+
+use Rhubarb\Leaf\Presenters\Application\Search\SearchPanelView;
+
+class BrowseSearchView extends SearchPanelView
+{
+	public function printViewContent()
+	{
+		?>
+			<div class="search-content">
+
+			</div>
+		<?php
+	}
+}
