98506c617e1cddd4047518c3bed31c01df917034
robmadeyou <robmadeyou@gmail.com>
2015-04-09 14:29:16 +0100

    actually pushing the changes...

--- src/Model/Container/PageContainer.php
+++ src/Model/Container/PageContainer.php
@@ -6,6 +6,27 @@
 namespace Cant\Phase\Me\Model\Container;
 
 
-class PageContainer {
+use Rhubarb\Stem\Models\Model;
+use Rhubarb\Stem\Repositories\MySql\Schema\Columns\AutoIncrement;
+use Rhubarb\Stem\Repositories\MySql\Schema\Columns\Int;
+use Rhubarb\Stem\Repositories\MySql\Schema\Columns\Varchar;
+use Rhubarb\Stem\Repositories\MySql\Schema\MySqlSchema;
 
+class PageContainer extends Model
+{
+
+	/**
+	 * Returns the schema for this data object.
+	 *
+	 * @return \Rhubarb\Stem\Schema\ModelSchema
+	 */
+	protected function createSchema()
+	{
+		$schema = new MySqlSchema( "tblPageContainer" );
+		$schema->addColumn(
+			new AutoIncrement( "PageContainerID" ),
+			new Varchar( "Url", 255 ),
+			new Int( "Priority" )
+		);
+	}
 }
