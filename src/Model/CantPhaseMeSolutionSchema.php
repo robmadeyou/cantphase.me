436647dd672917dd93f4293682b0088568609de7
robmadeyou <robmadeyou@gmail.com>
2015-05-27 13:50:25 +0100

    Starting to add models for Playlists while creating relationships for each

--- src/Model/CantPhaseMeSolutionSchema.php
+++ src/Model/CantPhaseMeSolutionSchema.php
@@ -17,10 +17,21 @@ class CantPhaseMeSolutionSchema extends SolutionSchema
 		$this->addModel( "MusicFavorite", __NAMESPACE__ . '\Music\MusicFavorite' );
 		$this->addModel( "PageContainer", __NAMESPACE__ . '\Container\PageContainer' );
 		$this->addModel( "Visit",         __NAMESPACE__ . '\Visit' );
+		$this->addModel( "Playlist",      __NAMESPACE__ . '\Music\Playlist' );
+		$this->addModel( "PlaylistSongs", __NAMESPACE__ . '\Music\PlaylistSongs' );
 	}
 
 	protected function defineRelationships()
 	{
 		parent::defineRelationships();
+
+		$this->declareManyToManyRelationships(
+			[
+				"Playlist" =>
+				[
+					"Songs" => "PlaylistSongs.PlaylistID_Music.MusicID:Playlists"
+				]
+			]
+		);
 	}
 }
