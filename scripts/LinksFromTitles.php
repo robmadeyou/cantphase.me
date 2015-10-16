74171dc5827d73d14319a57c56f33938c548fdc3
robmadeyou <robmadeyou@gmail.com>
2015-10-16 17:25:56 +0100

    Use google api instead of duckduckgo

--- scripts/LinksFromTitles.php
+++ scripts/LinksFromTitles.php
@@ -1,7 +1,7 @@
 <?php
 include( __DIR__ . '/simple_html_dom.php' );
 
-$mix = 'https://youtu.be/OvWKklczcsk';
+$mix = 'https://youtu.be/yX9qwWYAk7E';
 
 $Songs = [];
 exec( 'youtube-dl --get-description ' . $mix, $Songs );
@@ -12,14 +12,9 @@ foreach( $Songs as $song )
 		continue;
 	}
 	var_dump( $song );
-	$page = file_get_html( "https://duckduckgo.com/html/?q=" . urlencode( $song ) );
-
-	foreach( $page->find( 'div.links_main' ) as $element )
-	{
-		$url = "http://" . str_replace( ' ', '', $element->find( 'div.url', 0 )->plaintext );
-		$output = shell_exec( "youtube-dl -x --audio-quality 0 -i --add-metadata --write-thumbnail --prefer-avconv -o '" . __DIR__ . "/../tmp/%(id)s !i! %(uploader)s !i! %(title)s.%(ext)s' " . escapeshellarg( $url ) );
-		print $output;
-		continue 2;
-	}
+	$page = file_get_contents( "http://ajax.googleapis.com/ajax/services/search/video?q=" . urlencode( $song ) . "&v=1.0" );
+	$url = str_replace( ' ', '', json_decode( $page )->responseData->results[0]->url );
+	$output = shell_exec( "youtube-dl -x --audio-quality 0 -i --add-metadata --write-thumbnail --prefer-avconv -o '" . __DIR__ . "/../tmp/%(id)s !i! %(uploader)s !i! %(title)s.%(ext)s' " . escapeshellarg( $url ) );
+	print $output;
 }
 
